d7d9abc68f8e986de6157d37d9e9158fca9de1f8
unknown <kazuhoh12@gmail.com>
2025-01-25 20:28:04 +0900

    actionでデータを登録し、actionで取得したdataは再レンダリングされるが、usersは再レンダリングされない

--- apps/web/app/routes/users/route.tsx
+++ apps/web/app/routes/users/route.tsx
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client/core';
 import { client } from "~/root"
-import { useLoaderData } from '@remix-run/react';
+import { Form, redirect, useActionData, useLoaderData } from '@remix-run/react';
 
 const getUsers = gql`
   query GetTodos {
@@ -10,20 +10,52 @@ const getUsers = gql`
     }
   }
 `;
-
 export async function loader() {
     const { data } = await client.query({ query: getUsers });
     return { users: data.users };
 }
-
 type LoaderData = {
-    users: {id:string,name:string}[]
+    users: { id: string, name: string }[]
+}
+
+const createUser = gql`
+    mutation CreateUser($name: String!) {
+        createUser(name: $name){
+            id
+            name
+        }
+    }
+`
+export const action = async ({ request }: { request: Request }) => {
+    const formData = await request.formData();
+    const name = formData.get('name');
+
+    // Apollo Client を使って Mutation を呼び出す
+    const { data } = await client.mutate({
+        mutation: createUser,
+        variables: { name },
+    });
+
+    return data.createUser;
+};
+
+type ActionData = {
+    id: string, name: string
 }
 
 export default function App() {
+    // createdUserだけは再レンダリングされるが
+    // usersは再レンダリングされない
     const { users } = useLoaderData<LoaderData>();
+    const createdUser = useActionData<typeof action>();
     return (
-        <div>            
+        <div>
+            {createdUser && `${createdUser.name}が作成されました` }
+            <Form method="post">
+                <input type="text" name="name" required />
+                <button type="submit">userを登録する</button>
+            </Form>
+            <hr />
             {users.map((user) => (
                 <div key={user.id}>{user.name}</div>
             ))}
