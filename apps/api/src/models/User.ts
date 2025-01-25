4cb79804310ca89458890313d8e9f9950db4df56
unknown <kazuhoh12@gmail.com>
2025-01-25 19:31:29 +0900

    フォーマット

--- apps/api/src/models/User.ts
+++ apps/api/src/models/User.ts
@@ -2,30 +2,30 @@ import { builder } from "../builder"
 import { prisma } from "../db"
 
 builder.prismaObject('User', {
-    fields: t => ({        
+    fields: t => ({
         id: t.exposeID("id"),
         name: t.exposeString("name"),
     })
 })
 
-builder.queryField('users', (t) => 
+builder.queryField('users', (t) =>
     t.prismaField({
-        type:['User'],
-        resolve: async (query,root, args,ctx,info) => {
-            return prisma.user.findMany({...query})
+        type: ['User'],
+        resolve: async (query, root, args, ctx, info) => {
+            return prisma.user.findMany({ ...query })
         }
     })
 )
-builder.mutationField('createUser',(t) => 
+builder.mutationField('createUser', (t) =>
     t.prismaField({
-        type:'User',
-        args:{
-            name: t.arg.string({required: true})
+        type: 'User',
+        args: {
+            name: t.arg.string({ required: true })
         },
-        resolve: (query,root,args,ctx,info) => {
+        resolve: (query, root, args, ctx, info) => {
             return prisma.user.create({
                 ...query,
-                data:{
+                data: {
                     name: args.name,
                 }
             })
